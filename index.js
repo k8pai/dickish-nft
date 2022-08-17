b0fdcf675d24413cbeec6f138727d0983c09cf83
k8pai <thek8pai@gmail.com>
2022-08-17 17:05:58 +0530

    added images and animations

--- index.js
+++ index.js
@@ -1,10 +1,29 @@
+function contentAnimation(){
+  var t1 = gsap.timeline();
+  t1.to(".d", {duration: 1, transformOrigin: "bottom", rotate: "5deg", easing: "sine.inOut", repeat: -1, yoyo: true}, "-=1")
+  t1.to(".i", {duration: 1, transformOrigin: "bottom", rotate: "5deg", easing: "sine.inOut", repeat: -1, yoyo: true}, "-=.9")
+  t1.to(".c", {duration: 1,  transformOrigin: "bottom", rotate: "5deg", easing: "sine.inOut", repeat: -1, yoyo: true}, "-=.8")
+  t1.to(".k", {duration: 1, transformOrigin: "bottom", rotate: "5deg", easing: "sine.inOut", repeat: -1, yoyo: true}, "-=.7")
+  t1.to(".ii", {duration: 1,  transformOrigin: "bottom", rotate: "5deg", easing: "sine.inOut", repeat: -1, yoyo: true}, "-=.6")
+  t1.to(".s", {duration: 1, transformOrigin: "bottom", rotate: "5deg", easing: "sine.inOut", repeat: -1, yoyo: true}, "-=.5")
+  t1.to(".h", {duration: 1,  transformOrigin: "bottom", rotate: "5deg", easing: "sine.inOut", repeat: -1, yoyo: true}, "-=.4")
+  // t1.to(".d", {duration: 1, y: -10, easing: "sine.inOut", repeat: -1, yoyo: true}, "-=1")
+  // t1.to(".i", {duration: 1, y: -10, easing: "sine.inOut", repeat: -1, yoyo: true}, "-=.9")
+  // t1.to(".c", {duration: 1, y: -10, easing: "sine.inOut", repeat: -1, yoyo: true}, "-=.8")
+  // t1.to(".k", {duration: 1, y: -10, easing: "sine.inOut", repeat: -1, yoyo: true}, "-=.7")
+  // t1.to(".ii", {duration: 1, y: -10, easing: "sine.inOut", repeat: -1, yoyo: true}, "-=.6")
+  // t1.to(".s", {duration: 1, y: -10, easing: "sine.inOut", repeat: -1, yoyo: true}, "-=.5")
+  // t1.to(".h", {duration: 1, y: -10, easing: "sine.inOut", repeat: -1, yoyo: true}, "-=.4")
+}
+
+addEventListener("load", () =>{
+  contentAnimation();
+});
 document.querySelector('.connect-btn').addEventListener('click', event => {
     let account;
-    // let button = event.target;
     ethereum.request({method: 'eth_requestAccounts'}).then(accounts => {
       account = accounts[0];
       console.log(account);
-    //   button.textContent = account;
 
       ethereum.request({method: 'eth_getBalance' , params: [account, 'latest']}).then(result => {
         console.log(result);
